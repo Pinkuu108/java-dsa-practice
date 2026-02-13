2a7353327d860dff5219590978ec14f1e429abe3
unknown <pinkunaprusty108@gmail.com>
2026-02-13 23:38:34 +0530

    updated added

---
+++ src/common/interview/code/Reverse_String_Using_StringBuffer.java
@@ -0,0 +1,10 @@
+package common.interview.code;
+
+public class Reverse_String_Using_StringBuffer {
+public static void main(String[] args) {
+	
+	String str = "Chinu";
+	StringBuffer b=new StringBuffer(str);
+	System.out.println("Reverse_String_Using_StringBuffer.main():"+b.reverse());
+}
+}
