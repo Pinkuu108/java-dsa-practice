a3aed77afdb57e79119d3bcdbbaaf48231efd00d
unknown <pinkunaprusty108@gmail.com>
2026-02-13 23:35:54 +0530

    updated added

---
+++ src/common/interview/code/Reverse_String.java
@@ -0,0 +1,13 @@
+package common.interview.code;
+
+public class Reverse_String {
+	public static void main(String[] args) {
+		String str = "Chinu";
+		String rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+		}
+		System.out.println("Reverse_String.main():" + rev);
+	}
+
+}
