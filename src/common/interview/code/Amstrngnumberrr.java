f8e2eec58585f492f615ed33bdd6d8199106f7a4
unknown <pinkunaprusty108@gmail.com>
2026-02-20 23:54:46 +0530

    17 first commit

--- src/common/interview/code/Amstrngnumberrr.java
+++ src/common/interview/code/Amstrngnumberrr.java
@@ -3,14 +3,27 @@ package common.interview.code;
 public class Amstrngnumberrr {
 
 	public static void main(String[] args) {
-		String a = "pinkuna";
-		String b="chinu";
-		for(int i=0;i<a.length();i++) {
-			
+		String a = "pinku4";
+		
+		boolean p=false;
+		
+		for(int i=0;i<a.length();i++)
+		{
 			char ch=a.charAt(i);
-			
-			
+			if(ch>='0'&&ch<='9')
+			{
+				p=true;
+				break;
+			}
 		}
+		if(p==false)
+		{
+			System.out.println("no");
+		}else
+		{
+			System.out.println("yes");
+		}
+		
 		
 		
 
